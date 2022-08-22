0f4c93716afd0b9b6467a0a20f2b2c2579482bc6
Priyankalk1 <Priyankalk@maveric-systems.com>
2022-08-22 22:03:33 +0530

    Initial commit

---
+++ src/test/java/com/maveric/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maveric.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
